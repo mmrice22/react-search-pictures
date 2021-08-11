97fc76cc65184ebf2c33ab637228cda0fc25d7b6
Melissa Rice <melissa.rice@perficient.com>
2021-08-11 09:29:26 -0400

    onSearchSubmit uses async and await and arrow function to retrieve response

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,26 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios.get("https://api.unsplash.com/search/photos", {
+  // we know we are getting an array back, so init to empty array
+  state = { images: [] };
+
+   onSearchSubmit = async (term) => {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
       headers: {
         Authorization: "Client-ID pUK3paSBhE03jx8azL1hUb-sHBnOi0VMubNNE5lk1BU",
       },
     });
+    // console.log(response);
+    // console.log(response.data.results);
+    this.setState({ images: response.data.results });
   }
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images 
       </div>
     );
   }
